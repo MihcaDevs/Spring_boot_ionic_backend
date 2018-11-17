d4a3fcd34a6fa6a3a4a2e68b80c63c53387c07bc
Alberto Costa <mihcadevs@gmail.com>
2018-11-17 01:41:09 -0200

    Banco de dados H2 e criação automarica da base de dados categotia

--- src/main/java/com/albertocosta/cursomc/domain/Categoria.java
+++ src/main/java/com/albertocosta/cursomc/domain/Categoria.java
@@ -2,11 +2,22 @@ package com.albertocosta.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity // Indica que esta classe vai ser uma entidade do JPA
+
 public class Categoria  implements Serializable {
-	
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)  //Define a estrategia de geração automatica dos ID das categorias
 	private Integer id;
 	private String nome;
+	
 	
 	public Categoria() {
 		super();
